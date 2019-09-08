a8f2d397516770c2afc8440e921b4e2a72657266
Kevin Mai <kmai270@gmail.com>
2019-09-08 19:03:28 -0400

    Starting six card project

---
+++ SixCard/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace SixCard
+{
+    public class Startup
+    {
+        private DiscordSocketClient _Client;
+        private CommandService _Commands;
+        private IServiceProvider _Services;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public async Task RunBotAsync()
+        {
+            _Client = new DiscordSocketClient();
+            _Commands = new CommandService();
+
+            _Services = new ServiceCollection()
+                .AddSingleton(_Client)
+                .AddSingleton(_Commands)
+                .BuildServiceProvider();
+        }
+    }
+}
