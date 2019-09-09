2c00229c327b354cc422de4905ef742b80afbc8b
Kevin Mai <kmai270@gmail.com>
2019-09-08 21:47:13 -0400

    Updating card generation

--- SixCard/CardService.cs
+++ SixCard/CardService.cs
@@ -19,7 +19,7 @@ namespace SixCard
         {
             foreach (var suit in (Suits[])Enum.GetValues(typeof(Suits)))
             {
-                for (int i = 0; i < 12; i++)
+                for (int i = 0; i < 13; i++)
                 {
                     Cards.Add(new Card(i, suit));
                 }
--- Tests/CardServiceTests.cs
+++ Tests/CardServiceTests.cs
@@ -5,6 +5,7 @@ using System;
 
 namespace Tests
 {
+    [TestFixture]
     public class CardServiceTests
     {
         CardService _Sut;
