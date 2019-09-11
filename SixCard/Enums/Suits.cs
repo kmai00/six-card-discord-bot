2eb7bb1a1568ab9e7486ef5575a318f0c529c20e
Kevin Mai <kmai270@gmail.com>
2019-09-10 22:20:05 -0400

    Remove unneeded attribute

--- SixCard/Enums/Suits.cs
+++ SixCard/Enums/Suits.cs
@@ -4,13 +4,9 @@ namespace SixCard.Enums
 {
     public enum Suits
     {
-        [Description(":hearts:")]
         HEARTS = 1,
-        [Description(":diamonds:")]
         DIAMONDS = 2,
-        [Description(":clubs:")]
         CLUBS = 3,
-        [Description(":spades:")]
         SPADES = 4
     }
 }
